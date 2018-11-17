5f747b23657c38548ed7d7e002efb7f9d06d12c6
Tony DiPesa <adipesa@wellesley.edu>
2018-11-16 23:27:42 -0500

    Add files via upload

--- CreateMoviesJSON.sql
+++ CreateMoviesJSON.sql
@@ -4,10 +4,19 @@ go
 /******************************************************************************
 <historylog>
     <log revision="1.0" date="10/05/2018" bug="" email="adipesa@wellesley.edu"></log>
+    <log revision="1.1" date="11/16/2018" bug="" email="adipesa@wellesley.edu">Added drop if exists and count.</log>
 </historylog>
 ******************************************************************************/
 
-drop table dbo.MoviesJSON;
+if exists (
+    select *
+    from
+        sys.tables
+    where
+        object_id = object_id(N'dbo.MoviesJSON'))
+begin
+    drop table dbo.MoviesJSON
+end
 go
 
 create table dbo.MoviesJSON
@@ -16,3 +25,9 @@ create table dbo.MoviesJSON
  MoviesJSONNoArrayWrapper	nvarchar(max) null,
  InsertedDate			datetime not null default getdate());
 go
+
+/* Insert any indexes here */
+
+
+select count(*) as MoviesJSONRowCount
+from dbo.MoviesJSON;
